f15a2c31879f9e808f2dd2405af8ce7e884250d2
LMikeH <lmichaelhuang3@gmail.com>
2018-06-07 20:57:30 -0400

    implemented addNeuron

--- FeedForward.py
+++ FeedForward.py
@@ -31,7 +31,52 @@ class feedForward():
             out.append(o.activate())
         return out
 
+    def addNeuron(self):
+        # create new neuron and two new connections to replace random connection
+        neuron = nr.neuron(nr.neurotype.hidden)
+        con = random.choice(self.connection_list)
+        con.active = False;
+        conInput = nr.connection(con.parent_neuron, neuron)
+        conOutput = nr.connection(neuron, con.child_neuron)
+        conInput.w = 1
+        conOutput.w = con.w
+        # update the parent and child connections of the two old neurons and new neuron
+        con.parent_neuron.child_connections.append(conInput)
+        neuron.parent_connections.append(conInput)
+        neuron.child_connections.append(conOutput)
+        con.child_neuron.parent_connections.append(conOutput)
+        # append new connections and neuron to lists
+        self.connection_list.append(conInput)
+        self.connection_list.append(conOutput)
+        self.hidden.append(neuron)
+
+    def addConnection(self):
+        neuron1 = random.choice()
+        neuron2 = ramdom.choice()
+        newcon = nr.connection(neuron1, neuron2)
+        neuron1.child_connections.append(newcon)
+        neuron2.parent_connections.append(newcon)
+        self.connection_list.append(newcon)
 
 ins = 2*np.random.rand(10) - 1
 nn = feedForward(10,10)
+print(ins)
 print(nn.activate(ins))
+
+print(len(nn.hidden))
+print(len(nn.connection_list))
+nn.addNeuron()
+print(len(nn.hidden))
+print(len(nn.connection_list))
+
+neuron1 = nn.inlayer[0]
+neuron2 = nn.hidden[0]
+nn.addConnection()
+print(len(nn.connection_list))
+neuron3 = nn.hidden[0]
+neuron4 = nn.outlayer[3]
+nn.addConnection()
+print(len(nn.connection_list))
+print(nn.activate(ins))
+
+nn.addNeuron()
